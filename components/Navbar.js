8054c45f673807d8740f198ab88aa099a5d81231
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-04-29 23:09:06 +0500

    :construction: working on the navbar

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>My Blog</h1>
+            <div className="links">
+                <p>This is where the links go.</p>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
