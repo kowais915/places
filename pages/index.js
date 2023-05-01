1fab43d99946dafd2d32c34ab60fa49e53599e8b
pc <pc@pcs-MacBook-Pro.local>
2023-05-01 12:52:46 +0500

    :construction: working on fetching data from contentful

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,46 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '../components/Navbar'
+import {  createClient } from 'contentful' 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export async function getStaticProps(){
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY
+    
+
+  })
+
+  const res = await client.getEntries({ content_type: process.env.CONTENFUL_CONTENT_TYPE})
+  console.log(res)
+
+
+  return {
+    props: { place: res.items }
+  }
+}
+
+export default function Home({ place}) {
+  console.log(place)
   return (
     <>
       
       <Navbar/>
       <h1>This is places</h1>
+      {place.map((place) => {
+        return (
+          <div key={place.sys.id}>
+            <h2>{place.fields.place}</h2>
+            
+            
+          </div>
+        )
+      })}
+
+      
 
     </>
   )
