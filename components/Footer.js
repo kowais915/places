9662c58c954094356f30076f2e660566dbe81e05
kowais915 <kowais915@gmail.com>
2023-05-23 00:09:29 +0500

    :heavy_plus_sign: added font-awesome

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import styles from '@/styles/Footer.module.css'
 const Footer = () => {
     return ( 
         <footer className={styles.container}>
-            <p>CopyRight 2021</p>
+            <p>CopyRight 2023</p>
         </footer>
      );
 }
