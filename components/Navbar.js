72d4f2076ce785155027560e77f2cf2f82466d7d
kowais915 <kowais915@gmail.com>
2023-05-23 00:18:18 +0500

    :lipstick: styled the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,9 @@
 import Link  from 'next/link'
+import styles from '@/styles/Navbar.module.css'
 
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
+        <nav className={styles.container}>
             <h1>Places</h1>
             <div className="links">
                  <Link href="/" className='my-link'>Home</Link>
