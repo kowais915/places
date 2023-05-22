0ca8ce9cfb289fb39ff7a9f9ab55ec938b3631ee
kowais915 <kowais915@gmail.com>
2023-05-23 00:02:25 +0500

    :construction: working on the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
+import styles from '@/styles/Footer.module.css'
 const Footer = () => {
     return ( 
-        <footer>
+        <footer className={styles.container}>
             <p>CopyRight 2021</p>
         </footer>
      );
